491c3dfd45d7696b681076186e9420c0db189db2
schumach <schumach@stu-adm2.bmi.csc.fi>
2012-05-31 10:25:02 +0300

    local changes pre hackathon

--- perl/Pileup.pl
+++ perl/Pileup.pl
@@ -9,51 +9,18 @@
 # grunt> DEFINE CMD `Pileup.pl` ship('perl/Pileup.pl');
 # grunt> OP = stream B through CMD;
 
+sub computePileup {
+  my ($i, $reads) = @_;
 
-# adapted from Bio::DB::Bam::AlignWrapper
-sub getRefBases {
-    my ($qseq, $cigar_str, $md) = @_;
-
-    my @cigar;
-    my @cigart = split /(\d+[IDCHM])/, $cigar_str;
-    map { my $temp = $_; $temp =~ /(\d+[IDCHM])/; if (defined($1)) { push @cigar, $1; } } @cigart;
-
-    if ($md ne "") {  # try to use MD string
-
-        #preprocess qseq using cigar array
-        my $seq   = '';
-        foreach (@cigar) {
-	    m/(\d+)([IDCHM])/;
-            my ($operation,$count) = ($2, $1);
-            
-	    if ($operation eq 'M') {
-                $seq .= substr($qseq,0,$count,''); # include these residues
-            } elsif ($operation eq 'S' or $operation eq 'I') {
-                substr($qseq,0,$count,'');         # skip soft clipped and inserted residues
-            }
-        }
-
-        my $start = 0;
-        my $result;
-        while ($md =~ /(\d+)|\^([gatcn]+)|([gatcn]+)/ig) {
-            if (defined $1) {
-                $result .= substr($seq,$start,$1);
-                $start  += $1;
-            } elsif ($2) {
-                $result .= $2;
-            } elsif ($3) {
-                $result .= $3;
-                $start  += length $3;
-            }
-        }
-        return $result;
-    }
+  return scalar(@$reads);
 }
 
-# example input line:
-# SRR096689.6740187,115327100,115327149,GAGGAGAATAACTACACTTTAATTTTTTTTTAATTACAGTCACAAGTGAC,50M,:;::=;>?>=@<A>=B=AAA?@><<AAA;<A<!!2.5!!/3=!!129!!S,1024,0,96,0,11720,-1,10,[AS#2000,RG#0,HI#1,CS#T12202220330123111200303000000003000311112110021131,OQ#L````````````````````_^TS```ST`_!!9;T!!9O\!!@N[!!(,XA#3,NH#4,XE#---------------------------------0----1----0----3-,NM#0,CQ#!BBBBBBBBBABABBABAB??A5@+?BA=->=9%*&,?1%+;8@%23?5(,PG#bfast,IH#1,CM#4,MD#50]
-
+@reads = ();
+$prev_index = -1;
 $counter = 0;
+$start_index = -1;
+
+# note: we now assume first start index of bucket then end index of bucket, then number of reads in bucketi, then read itself, then reference base then attributes are given
 
 while (<>) {
 	chomp;
@@ -62,19 +29,110 @@ while (<>) {
 	my @samrec = split /\t/,$tmpar[0];
 	my @attributes = split /\t/, join('\t', splice(@tmpar, 1));
 	my $md_val = "";
-	my $name = $samrec[0];
-	my $cigar = $samrec[4];
+	#my @outBag = ();
+	
+	my $reference_bases = $samrec[16]; # reference bases that are "covered" by this read
+	my $start_index = $samrec[0]; #number of reads in bucket to be processed by this UDF
+	my $end_index = $samrec[1];
+	my $bucket_size = $samrec[2];
 
-	foreach (@attributes) {
-		if(/^MD\#/) {
-			s/\]//g;
-			$md_val = substr $_,3;
-			break;	
-		}
+	@samrec = splice(@samrec, -1*(scalar(@samrec)-3)); # cut off extra fields
+
+        my $start_this_read = $samrec[1];
+
+        if($prev_index == -1) {
+           if($start_this_read > $start_index) {
+              $prev_index = $start_this_read;
+           } else {
+              $prev_index = $start_index;
+	   }
+        }
+
+
+        if(scalar(@reads) > 0) {
+           # first, get rid of all reads that end before this one starts (there cannot be
+           # any more reads starting before this one since they arrive in the order of
+           # start coordindate by assumption)
+           while(scalar(@reads) > 0 && $reads[0][2] <  $start_this_read) {
+              $end_cur_read = $reads[0][2];
+
+              foreach my $i ($prev_index..($end_cur_read+1)) {
+                 my @tup=($i, computePileup($i, \@reads));
+                 #push @outBag, \@tup;
+                 print join(",",@tup);
+	      }
+
+              @reads = splice(@reads, -1*(scalar(@reads)-1)); # remove first read
+              $prev_index = $end_cur_read + 1;
+	   }
+
+           # note: the number of "active" reads has not changed since prev_index and start_this_read,
+           # so we produce the same output for this range of coordinates
+           foreach my $i (prev_index..$start_this_read) {
+              my @tup=($i, computePileup($i, \@reads));
+              #push @outBag, \@tup;
+              print join(",",@tup);
+	   }
+
+           # now place this read in list at a position according to its end coordinate
+           # if it is not alreay in the list (maybe both start and end coordinate lie in
+           # our bucket)
+           # NOTE: we now assume that ther are NO DUPLICATES (i.e., DISTINCT has been called)
+           #if samrec not in reads:
+           #if not any(r for r in reads if r[0] == samrec[0] and r[1] == samrec[1] and r[2] == samrec[2]):
+           #threshl = [$samrec[2]]*len(reads)
+	   my @smaller_reads = ();
+	   my @larger_reads = ();
+
+	   foreach $read (@reads) {
+		push @smaller_reads, $read if($$read[2] <= $samrec[2]);
+		push @larger_reads, $read if($$read[2] > $samrec[2]);
+	   }
+
+           #smaller_reads = map(filterFuncSmall, reads, threshl)
+           #larger_reads = map(filterFuncLarge, reads, threshl)
+           #
+           push @smaller_reads, \@samrec;
+	   @reads = @smaller_reads;
+	   push @reads, @larger_reads;
+
+           #smaller_reads.append(samrec)
+           #smaller_reads.extend(larger_reads)
+           #reads = filter(None, smaller_reads)
+        } else {
+           push @reads, \@samrec;
 	}
 
-	my $ref_bases = getRefBases($samrec[3],$cigar,$md_val);
+        #counter = counter + 1
+        $counter++;
 
-	print "$samrec[0],$cigar,$md_val,$ref_bases,$counter\n";
-	$counter++;
+        if($start_this_read > $prev_index) {
+           $prev_index = $start_this_read;
+	}
+
+        # if this is the last read of the bucket, clear the reads list in order of end
+        # coordinate and produce output
+        if($counter == $bucket_size) {
+           while(scalar(@reads) > 0) {
+              $end_cur_read = $reads[0][2];
+
+              if($end_cur_read >= $end_index) {
+                 $end_cur_read = $end_index - 1;
+	      }
+
+	      foreach my $i ($prev_index..($end_cur_read+1)) {
+                 @tup=($i, computePileup($i, \@reads));
+		 print join(",",@tup);
+                 #outBag.append(tup)
+              }
+
+              #reads = reads[1:]
+	      @reads = splice(@reads, -1*(scalar(@reads)-1)); # remove first read
+              $prev_index = $end_cur_read + 1;
+
+              if($prev_index >= $end_index) {
+                 break;
+	      }
+           }
+        }
 }
