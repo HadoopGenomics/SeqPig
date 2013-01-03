0771738a6370192dcf7c9e9aa31285419bb1069a
Andre Schumacher <andre.schumacher@aalto.fi>
2013-01-03 14:14:45 -0800

    removing dead code from BamUDFStorer

--- src/fi/aalto/seqpig/BamUDFStorer.java
+++ src/fi/aalto/seqpig/BamUDFStorer.java
@@ -103,24 +103,7 @@ public class BamUDFStorer extends StoreFunc {
 	    URI uri = new URI(samfileheaderfilename);
             FileSystem fs = FileSystem.get(uri, conf);
 	    
-	    /*try {
-		if(FileSystem.getDefaultUri(conf) == null
-		   || FileSystem.getDefaultUri(conf).toString() == "")
-		    fs = FileSystem.get(new URI("hdfs://"), conf);
-		else 
-		    fs = FileSystem.get(conf);
-	    } catch (Exception e) {
-		fs = FileSystem.get(new URI("hdfs://"), conf);
-            	System.out.println("WARNING: problems with filesystem config?");
-            	System.out.println("exception was: "+e.toString());
-	    }*/
-	    
-	    BufferedReader in;
-
-		//if(uri.getScheme() == null || uri.getScheme() == "")
-			//in = new BufferedReader(new InputStreamReader(fs.open(new Path(fs.getHomeDirectory(), new Path(samfileheaderfilename)))));
-		//else
-			in = new BufferedReader(new InputStreamReader(fs.open(new Path(samfileheaderfilename))));
+	    BufferedReader in = new BufferedReader(new InputStreamReader(fs.open(new Path(samfileheaderfilename))));
 	    
 	    while(true) {
 		str = in.readLine();
